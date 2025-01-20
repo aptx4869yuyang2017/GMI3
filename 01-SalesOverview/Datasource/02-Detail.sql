2a824189791e1be14aba20afe747c2044c279e0f
yuyang <aptx4869yuyang2017@gmail.com>
2025-01-20 10:22:59 +0800

    feat: Detail 更新逻辑
    新表 正式增加目标字段，数据粒度控制成财月

--- 01-SalesOverview/Datasource/02-Detail.sql
+++ 01-SalesOverview/Datasource/02-Detail.sql
@@ -4,28 +4,26 @@ WITH max_month AS
 	FROM vw_dim_gm_date_master
 	WHERE date_key = TO_CHAR(date_sub(GETDATE(), 1), 'yyyymmdd')
 	GROUP BY  fiscal_month_consecutive
-), dim_date AS
+), dim_date_monthly AS
 (
-	SELECT  date_key
-	       ,date_key_ly
-	       ,week_day
-	       ,lunar_date
-	       ,fiscal_year
+	SELECT  fiscal_year
 	       ,fiscal_month
 	       ,fiscal_quarter
 	       ,fiscal_yp
-	       ,fiscal_day_of_month
-	       ,LPAD(fiscal_day_of_month,2,'0')                                    AS fiscal_day
-	       ,fiscal_week_of_month
-	       ,fiscal_day_of_year
-	       ,fiscal_week_of_year
-	       ,CONCAT( 'F',SUBSTR(fiscal_year,-2),' P',SUBSTRING(fiscal_yp,5,2) ) AS fiscal_year_month
-	       ,int(fiscal_year * 12 + fiscal_month)                               AS fiscal_month_conse
-	       ,int( fiscal_year * 4 + int(SUBSTRING(fiscal_quarter,-1)) )         AS fiscal_quarter_conse
-	       ,CASE WHEN fiscal_month_consecutive < (
-	SELECT  fiscal_month_consecutive
-	FROM max_month ) THEN date_key_end_for_target_mtd else TO_CHAR(date_sub(GETDATE(), 1), 'yyyymmdd') end AS act_month_end
+	       ,CONCAT('F',SUBSTR(fiscal_year,-2),' P',SUBSTRING(fiscal_yp,5,2)) AS fiscal_year_month
+	       ,int(fiscal_year * 12 + fiscal_month)                             AS fiscal_month_conse
+	       ,int(fiscal_year * 4 + int(SUBSTRING(fiscal_quarter,-1)))         AS fiscal_quarter_conse
+	       ,MAX(date_key_end_for_target_mtd)                                 AS mtd_end
+	       ,MAX(date_key_end_for_target_qtd)                                 AS qtd_end
+	       ,MAX(date_key_end_for_target_ytd)                                 AS ytd_end
 	FROM vw_dim_gm_date_master
+	GROUP BY  fiscal_year
+	         ,fiscal_month
+	         ,fiscal_quarter
+	         ,fiscal_yp
+	         ,CONCAT('F',SUBSTR(fiscal_year,-2),' P',SUBSTRING(fiscal_yp,5,2))
+	         ,int(fiscal_year * 12 + fiscal_month)
+	         ,int(fiscal_year * 4 + int(SUBSTRING(fiscal_quarter,-1)))
 ) , time_past AS
 (
 	SELECT  fiscal_year
@@ -39,7 +37,8 @@ WITH max_month AS
 	         ,fiscal_month
 ) , fact AS
 (
-	SELECT  calendar_date
+	SELECT  fiscal_year
+	       ,fiscal_month
 	       ,product_brand_name
 	       ,business_area_name
 	       ,sales_district_name
@@ -52,49 +51,50 @@ WITH max_month AS
 	       ,product_subcategory_name
 	       ,product_strategy
 	       ,product_cate5_name
-	       ,is_new_product_flag
+	       ,new_prod_flag                                                                     AS is_new_product_flag
 	       ,CASE WHEN business_area_name = '零售' AND customer_group_3_name IN ('NKA','RKA','LKA') THEN customer_group_name
 	             WHEN business_area_name = '餐饮' AND customer_group_3_name = '直营' THEN customer_group_name
 	             WHEN business_area_name = '电商' THEN customer_group_name  ELSE payer_name END AS customer_name
 	       ,CAST(CAST(stat_weight AS BIGINT) AS STRING) || stat_weight_unit_name              AS stat_weight
-	       ,SUM(billing_qty)                                                                  AS cases
-	       ,SUM(list_price_revenue_excl_r100)                                                 AS gsv_comp
-	       ,SUM(billing_qty_ly)                                                               AS cases_ly
-	       ,SUM(list_price_revenue_excl_r100_ly)                                              AS gsv_comp_ly
-	       ,SUM(le_case_org)                                                                  AS le_case_org
-	       ,SUM(le_gsv_org)*1e3                                                               AS le_gsv_org
-	       ,SUM(le_case_org)                                                                  AS st_case_org
-	       ,SUM(le_gsv_org)*1e3                                                               AS st_gsv_org
-	       ,SUM(sp_case_org)                                                                  AS sp_case_org
-	       ,SUM(sp_gsv_org)*1e3                                                               AS sp_gsv_org
-	       ,SUM(le_case_customer)                                                             AS le_case_customer
-	       ,SUM(le_gsv_customer)*1e3                                                          AS le_gsv_customer
-	       ,SUM(sp_case_customer)                                                             AS sp_case_customer
-	       ,SUM(sp_gsv_customer)*1e3                                                          AS sp_gsv_customer
-	       ,SUM(le_case_customer)                                                             AS st_case_customer
-	       ,SUM(le_gsv_customer)*1e3                                                          AS st_gsv_customer
-	       ,SUM(le_case_product)                                                              AS le_case_product
-	       ,SUM(le_gsv_product)*1e3                                                           AS le_gsv_product
+	       ,SUM(sellin_case)                                                                  AS cases
+	       ,SUM(sellin_gsv)                                                                   AS gsv_comp
+	       ,SUM(sellin_case_ly)                                                               AS cases_ly
+	       ,SUM(sellin_gsv_ly)                                                                AS gsv_comp_ly
+	       ,SUM(sellin_le_case)                                                               AS le_case_org
+	       ,SUM(sellin_le_gsv)*1e3                                                            AS le_gsv_org
+	       ,SUM(sellin_bonus_target_case)                                                     AS st_case_org
+	       ,SUM(sellin_bonus_target_gsv)*1e3                                                  AS st_gsv_org
+	       ,SUM(0)                                                                            AS sp_case_org
+	       ,SUM(0)                                                                            AS sp_gsv_org
+	       ,SUM(0)                                                                            AS le_case_customer
+	       ,SUM(0)*1e3                                                                        AS le_gsv_customer
+	       ,SUM(0)                                                                            AS sp_case_customer
+	       ,SUM(0)*1e3                                                                        AS sp_gsv_customer
+	       ,SUM(0)                                                                            AS st_case_customer
+	       ,SUM(0)*1e3                                                                        AS st_gsv_customer
+	       ,SUM(0)                                                                            AS le_case_product
+	       ,SUM(0)*1e3                                                                        AS le_gsv_product
 	       ,0                                                                                 AS sp_case_product
 	       ,0                                                                                 AS sp_gsv_product
-	       ,SUM(stock_count_unit_qty)                                                         AS stock_case
-	       ,SUM(stock_gsv_mdm)                                                                AS stock_gsv
-	       ,SUM(stock_count_unit_qty_ly)                                                      AS stock_case_ly
-	       ,SUM(stock_gsv_mdm_ly)                                                             AS stock_gsv_ly
-	       ,SUM(sellout_past_28days_count_unit_qty)/28                                        AS sellout_past_case
-	       ,SUM(sellout_past_28days_gsv_mdm)/28                                               AS sellout_past_gsv
-	       ,SUM(sellout_next_28days_count_unit_qty)/28                                        AS sellout_next_case
-	       ,SUM(sellout_next_28days_gsv_mdm)/28                                               AS sellout_next_gsv
-	       ,SUM(sellout_past_28days_count_unit_qty_ly)/28                                     AS sellout_past_case_ly
-	       ,SUM(sellout_past_28days_gsv_mdm_ly)/28                                            AS sellout_past_gsv_ly
-	       ,SUM(sellout_next_28days_count_unit_qty_ly)/28                                     AS sellout_next_case_ly
-	       ,SUM(sellout_next_28days_gsv_mdm_ly)/28                                            AS sellout_next_gsv_ly
-	       ,SUM(count_unit_qty)                                                               AS sellout_case
-	       ,SUM(gsv_mdm)                                                                      AS sellout_gsv
-	       ,SUM(count_unit_qty_ly)                                                            AS sellout_case_ly
-	       ,SUM(gsv_mdm_ly)                                                                   AS sellout_gsv_ly
-	FROM tb_billing_data
-	GROUP BY  calendar_date
+	       ,SUM(stock_case)                                                                   AS stock_case
+	       ,SUM(stock_gsv)                                                                    AS stock_gsv
+	       ,SUM(stock_case_ly)                                                                AS stock_case_ly
+	       ,SUM(stock_gsv_ly)                                                                 AS stock_gsv_ly
+	       ,SUM(sellout_past_28days_case)/28                                                  AS sellout_past_case
+	       ,SUM(sellout_past_28days_gsv)/28                                                   AS sellout_past_gsv
+	       ,SUM(sellout_next_28days_case)/28                                                  AS sellout_next_case
+	       ,SUM(sellout_next_28days_gsv)/28                                                   AS sellout_next_gsv
+	       ,SUM(sellout_past_28days_case_ly)/28                                               AS sellout_past_case_ly
+	       ,SUM(sellout_past_28days_gsv_ly)/28                                                AS sellout_past_gsv_ly
+	       ,SUM(sellout_next_28days_case_ly)/28                                               AS sellout_next_case_ly
+	       ,SUM(sellout_next_28days_gsv_ly)/28                                                AS sellout_next_gsv_ly
+	       ,SUM(sellout_case)                                                                 AS sellout_case
+	       ,SUM(sellout_gsv)                                                                  AS sellout_gsv
+	       ,SUM(sellout_case_ly)                                                              AS sellout_case_ly
+	       ,SUM(sellout_gsv_ly)                                                               AS sellout_gsv_ly
+	FROM tb_sales_overview_detail_fact
+	GROUP BY  fiscal_year
+	         ,fiscal_month
 	         ,product_brand_name
 	         ,business_area_name
 	         ,sales_district_name
@@ -107,7 +107,7 @@ WITH max_month AS
 	         ,product_subcategory_name
 	         ,product_strategy
 	         ,product_cate5_name
-	         ,is_new_product_flag
+	         ,new_prod_flag
 	         ,CAST(CAST(stat_weight AS BIGINT) AS STRING) || stat_weight_unit_name
 	         ,CASE WHEN business_area_name = '零售' AND customer_group_3_name IN ('NKA','RKA','LKA') THEN customer_group_name
 	             WHEN business_area_name = '餐饮' AND customer_group_3_name = '直营' THEN customer_group_name
@@ -158,16 +158,22 @@ WITH max_month AS
 	WHERE business_area_name IN ('餐饮', '零售', '电商') 
 )
 SELECT  t1.*
-       ,CASE WHEN customer_name IN ('山姆','大润发','沃尔玛','华润','永辉','麦德龙','罗森','福满家','开市客') THEN customer_name
-             WHEN customer_name IN ('广州赛味谦食品有限公司','杭州鼎佳食品有限公司','深圳市东江鑫达食品有限公司','珠海市大南北发展有限公司','广州市炽发贸易有限公司','福州诚必隆贸易有限公司','宁波开明街坊食品有限公司','北京鸿昌硕商贸有限公司','长沙能臣食品贸易有限公司') THEN customer_name  ELSE 'others' END AS top_customer_name
-       ,t2.*
+       ,null                                                                 AS top_customer_name
+       ,t2.fiscal_quarter
+       ,t2.fiscal_yp
+       ,t2.fiscal_year_month
+       ,t2.fiscal_month_conse
+       ,t2.fiscal_quarter_conse
+       ,t2.mtd_end
+       ,t2.qtd_end
+       ,t2.ytd_end
        ,t3.mtd_time_pasting
        ,t3.qtd_time_pasting
        ,t3.ytd_time_pasting
-       ,CASE WHEN t1.calendar_date = t2.act_month_end THEN 'Y'  ELSE 'N' END AS act_month_end_filter
+       ,'Y' AS act_month_end_filter
 FROM fact_multi AS t1
-LEFT JOIN dim_date AS t2
-ON t1.calendar_date = t2.date_key
+LEFT JOIN dim_date_monthly AS t2
+ON t1.fiscal_year = t2.fiscal_year AND t1.fiscal_month = t2.fiscal_month
 LEFT JOIN time_past AS t3
 ON t2.fiscal_year = t3.fiscal_year AND t2.fiscal_month = t3.fiscal_month
 WHERE t2.fiscal_year >= 2025
