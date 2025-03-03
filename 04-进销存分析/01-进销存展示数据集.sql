aa3f9213d198f22bb28ab2a9fd3f1e5b5106fbb4
yuyang <aptx4869yuyang2017@gmail.com>
2025-03-03 15:36:41 +0800

    feat: sellin/sellout/inv

--- 04-进销存分析/01-进销存展示数据集.sql
+++ 04-进销存分析/01-进销存展示数据集.sql
@@ -214,6 +214,354 @@ WITH cte_current_fiscal_year AS
 	         ,fiscal_quarter
 	         ,business_area_name
 	         ,sales_district_name
+), cte_sellout AS
+(
+	SELECT  'History Act'     AS data_label
+	       ,'Month'           AS sub_year_label
+	       ,'Sell out'        AS theme_level
+	       ,fiscal_year       AS year_level
+	       ,fiscal_month_show AS sub_year_level
+	       ,business_area_name
+	       ,sales_district_name
+	       ,SUM(sellout_case) AS case_value
+	       ,SUM(sellout_gsv)  AS gsv_value
+	       ,0                 AS case_value_2
+	       ,0                 AS gsv_value_2
+	FROM tb_sales_overview_monthly_flat_qbi
+	WHERE mt <> ''
+	AND fiscal_year < (
+	SELECT  currenct_fiscal_year
+	FROM cte_current_fiscal_year)
+	GROUP BY  fiscal_year
+	         ,fiscal_month_show
+	         ,business_area_name
+	         ,sales_district_name
+	UNION ALL
+	SELECT  'History Act'     AS data_label
+	       ,'Quarter'         AS sub_year_label
+	       ,'Sell out'        AS theme_level
+	       ,fiscal_year       AS year_level
+	       ,fiscal_quarter    AS sub_year_level
+	       ,business_area_name
+	       ,sales_district_name
+	       ,SUM(sellout_case) AS case_value
+	       ,SUM(sellout_gsv)  AS gsv_value
+	       ,0                 AS case_value_2
+	       ,0                 AS gsv_value_2
+	FROM tb_sales_overview_monthly_flat_qbi
+	WHERE mt <> ''
+	AND fiscal_year < (
+	SELECT  currenct_fiscal_year
+	FROM cte_current_fiscal_year)
+	GROUP BY  fiscal_year
+	         ,fiscal_quarter
+	         ,business_area_name
+	         ,sales_district_name
+	UNION ALL
+	SELECT  'CY Act'              AS data_label
+	       ,'Month'               AS sub_year_label
+	       ,'Sell out'            AS theme_level
+	       ,fiscal_year || ' Act' AS year_level
+	       ,fiscal_month_show     AS sub_year_level
+	       ,business_area_name
+	       ,sales_district_name
+	       ,SUM(sellout_case)     AS case_value
+	       ,SUM(sellout_gsv)      AS gsv_value
+	       ,0                     AS case_value_2
+	       ,0                     AS gsv_value_2
+	FROM tb_sales_overview_monthly_flat_qbi
+	WHERE mt <> ''
+	AND fiscal_year = (
+	SELECT  currenct_fiscal_year
+	FROM cte_current_fiscal_year)
+	GROUP BY  fiscal_year || ' Act'
+	         ,fiscal_month_show
+	         ,business_area_name
+	         ,sales_district_name
+	UNION ALL
+	SELECT  'CY Act'              AS data_label
+	       ,'Quarter'             AS sub_year_label
+	       ,'Sell out'            AS theme_level
+	       ,fiscal_year || ' Act' AS year_level
+	       ,fiscal_quarter        AS sub_year_level
+	       ,business_area_name
+	       ,sales_district_name
+	       ,SUM(sellout_case)     AS case_value
+	       ,SUM(sellout_gsv)      AS gsv_value
+	       ,0                     AS case_value_2
+	       ,0                     AS gsv_value_2
+	FROM tb_sales_overview_monthly_flat_qbi
+	WHERE mt <> ''
+	AND fiscal_year = (
+	SELECT  currenct_fiscal_year
+	FROM cte_current_fiscal_year)
+	GROUP BY  fiscal_year || ' Act'
+	         ,fiscal_quarter
+	         ,business_area_name
+	         ,sales_district_name
+	UNION ALL
+	SELECT  'LY Act'              AS data_label
+	       ,'Month'               AS sub_year_label
+	       ,'Sell out'            AS theme_level
+	       ,fiscal_year || ' YOY' AS year_level
+	       ,fiscal_month_show     AS sub_year_level
+	       ,business_area_name
+	       ,sales_district_name
+	       ,SUM(sellout_case)     AS case_value
+	       ,SUM(sellout_gsv)      AS gsv_value
+	       ,SUM(sellout_case_ly)  AS case_value_2
+	       ,SUM(sellout_gsv_ly)   AS gsv_value_2
+	FROM tb_sales_overview_monthly_flat_qbi
+	WHERE mt <> ''
+	AND fiscal_year = (
+	SELECT  currenct_fiscal_year
+	FROM cte_current_fiscal_year)
+	GROUP BY  fiscal_year || ' YOY'
+	         ,fiscal_month_show
+	         ,business_area_name
+	         ,sales_district_name
+	UNION ALL
+	SELECT  'LY Act'              AS data_label
+	       ,'Quarter'             AS sub_year_label
+	       ,'Sell out'            AS theme_level
+	       ,fiscal_year || ' YOY' AS year_level
+	       ,fiscal_quarter        AS sub_year_level
+	       ,business_area_name
+	       ,sales_district_name
+	       ,SUM(sellout_case)     AS case_value
+	       ,SUM(sellout_gsv)      AS gsv_value
+	       ,SUM(sellout_case_ly)  AS case_value_2
+	       ,SUM(sellout_gsv_ly)   AS gsv_value_2
+	FROM tb_sales_overview_monthly_flat_qbi
+	WHERE mt <> ''
+	AND fiscal_year = (
+	SELECT  currenct_fiscal_year
+	FROM cte_current_fiscal_year)
+	GROUP BY  fiscal_year || ' YOY'
+	         ,fiscal_quarter
+	         ,business_area_name
+	         ,sales_district_name
+	UNION ALL
+	SELECT  'CY LE'              AS data_label
+	       ,'Month'              AS sub_year_label
+	       ,'Sell out'           AS theme_level
+	       ,fiscal_year || ' LE' AS year_level
+	       ,fiscal_month_show    AS sub_year_level
+	       ,business_area_name
+	       ,sales_district_name
+	       ,SUM(sellout_le_case) AS case_value
+	       ,SUM(sellout_le_gsv)  AS gsv_value
+	       ,0                    AS case_value_2
+	       ,0                    AS gsv_value_2
+	FROM tb_sales_overview_monthly_flat_qbi
+	WHERE mt <> ''
+	AND fiscal_year = (
+	SELECT  currenct_fiscal_year
+	FROM cte_current_fiscal_year)
+	GROUP BY  fiscal_year || ' LE'
+	         ,fiscal_month_show
+	         ,business_area_name
+	         ,sales_district_name
+	UNION ALL
+	SELECT  'CY LE'              AS data_label
+	       ,'Quarter'            AS sub_year_label
+	       ,'Sell out'           AS theme_level
+	       ,fiscal_year || ' LE' AS year_level
+	       ,fiscal_quarter       AS sub_year_level
+	       ,business_area_name
+	       ,sales_district_name
+	       ,SUM(sellout_le_case) AS case_value
+	       ,SUM(sellout_le_gsv)  AS gsv_value
+	       ,0                    AS case_value_2
+	       ,0                    AS gsv_value_2
+	FROM tb_sales_overview_monthly_flat_qbi
+	WHERE mt <> ''
+	AND fiscal_year = (
+	SELECT  currenct_fiscal_year
+	FROM cte_current_fiscal_year)
+	GROUP BY  fiscal_year || ' LE'
+	         ,fiscal_quarter
+	         ,business_area_name
+	         ,sales_district_name
+) , cte_inventory AS
+(
+	SELECT  'CY Inv'                     AS data_label
+	       ,'Month'                      AS sub_year_label
+	       ,'Inventory'                  AS theme_level
+	       ,fiscal_year || ' Inv Latest' AS year_level
+	       ,fiscal_month_show            AS sub_year_level
+	       ,business_area_name
+	       ,sales_district_name
+	       ,SUM(stock_case)              AS case_value
+	       ,SUM(stock_gsv)               AS gsv_value
+	       ,0                            AS case_value_2
+	       ,0                            AS gsv_value_2
+	FROM tb_sales_overview_monthly_flat_qbi
+	WHERE mt <> ''
+	AND fiscal_year = (
+	SELECT  currenct_fiscal_year
+	FROM cte_current_fiscal_year)
+	GROUP BY  fiscal_year || ' Inv Latest'
+	         ,fiscal_month_show
+	         ,business_area_name
+	         ,sales_district_name
+	UNION ALL
+	SELECT  'CY Inv'                     AS data_label
+	       ,'Quarter'                    AS sub_year_label
+	       ,'Inventory'                  AS theme_level
+	       ,fiscal_year || ' Inv Latest' AS year_level
+	       ,fiscal_quarter               AS sub_year_level
+	       ,business_area_name
+	       ,sales_district_name
+	       ,SUM(stock_case)              AS case_value
+	       ,SUM(stock_gsv)               AS gsv_value
+	       ,0                            AS case_value_2
+	       ,0                            AS gsv_value_2
+	FROM tb_sales_overview_monthly_flat_qbi
+	WHERE mt <> ''
+	AND fiscal_year = (
+	SELECT  currenct_fiscal_year
+	FROM cte_current_fiscal_year)
+	GROUP BY  fiscal_year || ' Inv Latest'
+	         ,fiscal_quarter
+	         ,business_area_name
+	         ,sales_district_name
+	UNION ALL
+	SELECT  'CY Inv'                   AS data_label
+	       ,'Month'                    AS sub_year_label
+	       ,'Inventory'                AS theme_level
+	       ,fiscal_year || ' Inv Last' AS year_level
+	       ,fiscal_month_show          AS sub_year_level
+	       ,business_area_name
+	       ,sales_district_name
+	       ,SUM(stock_case)            AS case_value
+	       ,SUM(stock_gsv)             AS gsv_value
+	       ,0                          AS case_value_2
+	       ,0                          AS gsv_value_2
+	FROM tb_sales_overview_monthly_flat_qbi
+	WHERE mt <> ''
+	AND fiscal_year = (
+	SELECT  currenct_fiscal_year
+	FROM cte_current_fiscal_year)
+	GROUP BY  fiscal_year || ' Inv Last'
+	         ,fiscal_month_show
+	         ,business_area_name
+	         ,sales_district_name
+	UNION ALL
+	SELECT  'CY Inv'                   AS data_label
+	       ,'Quarter'                  AS sub_year_label
+	       ,'Inventory'                AS theme_level
+	       ,fiscal_year || ' Inv Last' AS year_level
+	       ,fiscal_quarter             AS sub_year_level
+	       ,business_area_name
+	       ,sales_district_name
+	       ,SUM(stock_case)            AS case_value
+	       ,SUM(stock_gsv)             AS gsv_value
+	       ,0                          AS case_value_2
+	       ,0                          AS gsv_value_2
+	FROM tb_sales_overview_monthly_flat_qbi
+	WHERE mt <> ''
+	AND fiscal_year = (
+	SELECT  currenct_fiscal_year
+	FROM cte_current_fiscal_year)
+	GROUP BY  fiscal_year || ' Inv Last'
+	         ,fiscal_quarter
+	         ,business_area_name
+	         ,sales_district_name
+	UNION ALL
+	SELECT  'CY DOH'                      AS data_label
+	       ,'Month'                       AS sub_year_label
+	       ,'Inventory'                   AS theme_level
+	       ,fiscal_year || ' DOH Latest'  AS year_level
+	       ,fiscal_month_show             AS sub_year_level
+	       ,business_area_name
+	       ,sales_district_name
+	       ,SUM(stock_case)               AS case_value
+	       ,SUM(stock_gsv)                AS gsv_value
+	       ,SUM(sellout_next_28days_case) AS case_value_2
+	       ,SUM(sellout_next_28days_gsv)  AS gsv_value_2
+	FROM tb_sales_overview_monthly_flat_qbi
+	WHERE mt <> ''
+	AND fiscal_year = (
+	SELECT  currenct_fiscal_year
+	FROM cte_current_fiscal_year)
+	GROUP BY  fiscal_year || ' DOH Latest'
+	         ,fiscal_month_show
+	         ,business_area_name
+	         ,sales_district_name
+	UNION ALL
+	SELECT  'CY DOH'                      AS data_label
+	       ,'Quarter'                     AS sub_year_label
+	       ,'Inventory'                   AS theme_level
+	       ,fiscal_year || ' DOH Latest'  AS year_level
+	       ,fiscal_quarter                AS sub_year_level
+	       ,business_area_name
+	       ,sales_district_name
+	       ,SUM(stock_case)               AS case_value
+	       ,SUM(stock_gsv)                AS gsv_value
+	       ,SUM(sellout_next_28days_case) AS case_value_2
+	       ,SUM(sellout_next_28days_gsv)  AS gsv_value_2
+	FROM tb_sales_overview_monthly_flat_qbi
+	WHERE mt <> ''
+	AND fiscal_year = (
+	SELECT  currenct_fiscal_year
+	FROM cte_current_fiscal_year)
+	GROUP BY  fiscal_year || ' DOH Latest'
+	         ,fiscal_quarter
+	         ,business_area_name
+	         ,sales_district_name
+	UNION ALL
+	SELECT  'CY DOH'                      AS data_label
+	       ,'Month'                       AS sub_year_label
+	       ,'Inventory'                   AS theme_level
+	       ,fiscal_year || ' DOH Last'    AS year_level
+	       ,fiscal_month_show             AS sub_year_level
+	       ,business_area_name
+	       ,sales_district_name
+	       ,SUM(stock_case)               AS case_value
+	       ,SUM(stock_gsv)                AS gsv_value
+	       ,SUM(sellout_next_28days_case) AS case_value_2
+	       ,SUM(sellout_next_28days_gsv)  AS gsv_value_2
+	FROM tb_sales_overview_monthly_flat_qbi
+	WHERE mt <> ''
+	AND fiscal_year = (
+	SELECT  currenct_fiscal_year
+	FROM cte_current_fiscal_year)
+	GROUP BY  fiscal_year || ' DOH Last'
+	         ,fiscal_month_show
+	         ,business_area_name
+	         ,sales_district_name
+	UNION ALL
+	SELECT  'CY DOH'                      AS data_label
+	       ,'Quarter'                     AS sub_year_label
+	       ,'Inventory'                   AS theme_level
+	       ,fiscal_year || ' DOH Last'    AS year_level
+	       ,fiscal_quarter                AS sub_year_level
+	       ,business_area_name
+	       ,sales_district_name
+	       ,SUM(stock_case)               AS case_value
+	       ,SUM(stock_gsv)                AS gsv_value
+	       ,SUM(sellout_next_28days_case) AS case_value_2
+	       ,SUM(sellout_next_28days_gsv)  AS gsv_value_2
+	FROM tb_sales_overview_monthly_flat_qbi
+	WHERE mt <> ''
+	AND fiscal_year = (
+	SELECT  currenct_fiscal_year
+	FROM cte_current_fiscal_year)
+	GROUP BY  fiscal_year || ' DOH Last'
+	         ,fiscal_quarter
+	         ,business_area_name
+	         ,sales_district_name
+), cte_res AS
+(
+	SELECT  *
+	FROM cte_sellout
+	UNION ALL
+	SELECT  *
+	FROM cte_sellin
+	UNION ALL
+	SELECT  *
+	FROM cte_inventory
 )
 SELECT  *
-FROM cte_sellin
+FROM cte_res
