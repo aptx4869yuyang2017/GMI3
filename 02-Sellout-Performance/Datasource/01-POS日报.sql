2348fb6c7e0835891af17dc56af6e5e52ade5b4a
yuyang <aptx4869yuyang2017@gmail.com>
2025-03-17 15:05:52 +0800

    debug: POS 日报 为了处理后端没法，先前端处理一下

--- 02-Sellout-Performance/Datasource/01-POS日报.sql
+++ 02-Sellout-Performance/Datasource/01-POS日报.sql
@@ -3,8 +3,8 @@ WITH cte_fact AS
 	SELECT  cast(to_date(report_date,'yyyymmdd')                                                                AS date) report_date
 	       ,business_area_name
 	       ,sales_district_name
-           ,customer_group_2_name
-           ,customer_group_5_name 
+	       ,customer_group_2_name
+	       ,customer_group_5_name
 	       ,product_brand_name
 	       ,product_category_name
 	       ,customer_type
@@ -35,28 +35,45 @@ WITH cte_fact AS
 	       ,week_day_ly
 	       ,week_day_lly
 	       ,holiday
-	       ,holiday_flag
+	       ,CASE WHEN holiday_flag = 10 THEN 5
+	             WHEN holiday_flag = 11 THEN 6
+	             WHEN holiday_flag = 12 THEN 7
+	             WHEN holiday_flag = 5 THEN 10
+	             WHEN holiday_flag = 6 THEN 11
+	             WHEN holiday_flag = 7 THEN 12  ELSE holiday_flag END                                           AS holiday_flag
 	       ,holiday_begin
 	       ,holiday_ly
-	       ,holiday_ly_flag
+	       ,CASE WHEN holiday_ly_flag = 10 THEN 5
+	             WHEN holiday_ly_flag = 11 THEN 6
+	             WHEN holiday_ly_flag = 12 THEN 7
+	             WHEN holiday_ly_flag = 5 THEN 10
+	             WHEN holiday_ly_flag = 6 THEN 11
+	             WHEN holiday_ly_flag = 7 THEN 12  ELSE holiday_ly_flag END                                     AS holiday_ly_flag
 	       ,holiday_begin_ly
 	       ,holiday_lly
-	       ,holiday_lly_flag
+	       ,CASE WHEN holiday_lly_flag = 10 THEN 5
+	             WHEN holiday_lly_flag = 11 THEN 6
+	             WHEN holiday_lly_flag = 12 THEN 7
+	             WHEN holiday_lly_flag = 5 THEN 10
+	             WHEN holiday_lly_flag = 6 THEN 11
+	             WHEN holiday_lly_flag = 7 THEN 12  ELSE holiday_lly_flag END                                   AS holiday_lly_flag
 	       ,holiday_begin_lly
 	       ,cast(to_date(substring(created_dt,1,10),'yyyy-mm-dd') AS date)                                      AS created_dt
 	       ,cast(date_add(to_date(substring(created_dt,1,10),'yyyy-mm-dd'),-1) AS date)                         AS create_dt_yesterday
 	       ,cast(date_add(to_date(substring(created_dt,1,10),'yyyy-mm-dd'),-2) AS date)                         AS create_dt_day_before
 	FROM tb_pos_sales_calendar_flat_qbi
-	WHERE dt >= '20241201'
+	WHERE dt >= '20240101'
 	AND product_brand_name IN ('湾仔码头' ， '哈根达斯')
 	AND business_area_name IN ('零售')
 	AND customer_name IN ('世纪联华', '华润万家', '农工商', '北京华联' , '大润发', '大统华', '天虹商场', '家家悦' , '山姆', '新华都', '易初莲花', '永辉' , '沃尔玛', '物美', '联华', '苏果', '麦德龙') 
 ) , cte_next_month AS
 (
-	SELECT  max(to_char(cast(add_months(date_add(to_date(substring(created_dt,1,10),'yyyy-mm-dd'),-2),1) AS date),'yyyyMM')) AS next_month
+	SELECT  MAX(to_char(cast(add_months(date_add(to_date(substring(created_dt,1,10),'yyyy-mm-dd'),-2),1) AS date),'yyyyMM')) AS next_month
 	FROM tb_pos_sales_calendar_flat_qbi
-	WHERE dt >= '20241201' 
+	WHERE dt >= '20240101' 
 )
 SELECT  *
 FROM cte_fact
-where substring(date_key, 1, 6) < (select next_month from cte_next_month )
+WHERE substring(date_key, 1, 6) < (
+SELECT  next_month
+FROM cte_next_month )
