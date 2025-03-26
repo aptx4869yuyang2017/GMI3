ed9d9808ed3a78c5d84488b0ddcf2b71e4868b2e
yuyang <aptx4869yuyang2017@gmail.com>
2025-03-26 13:23:27 +0800

    test: 进销存 overview 增加测试

--- 01-SalesOverview/SIT/03.销售总览-多来源核对-01 OverviewKPI - 进销存分析.sql
+++ 01-SalesOverview/SIT/03.销售总览-多来源核对-01 OverviewKPI - 进销存分析.sql
@@ -1,4 +1,4 @@
-WITH coverpage AS
+WITH OverviewKPI AS
 (
 	SELECT  product_brand_name
 	       ,business_area_name
@@ -7,12 +7,12 @@ WITH coverpage AS
 	FROM vw_sellin_coverpage_monthly_flat_qbi --销售总览-01OverviewKPI
 	WHERE date_range_type = 'MTD'
 	AND fiscal_year = 2025
-	AND fiscal_month IN (9, 10)
+	AND fiscal_month IN (6, 10)
 	AND product_brand_name IN ( '湾仔码头', '哈根达斯' )
 	GROUP BY  product_brand_name
 	         ,business_area_name
 	         ,fiscal_month
-), all AS
+), all_t AS
 (
 	SELECT  product_brand_name
 	       ,business_area_name
@@ -21,10 +21,25 @@ WITH coverpage AS
 	FROM tb_sales_overview_monthly_flat_qbi -- Non-Shop-进销存分析数据集
 	WHERE mt <> ''
 	AND fiscal_year = 2025
-	AND fiscal_month IN (9, 10)
+	AND fiscal_month IN (6, 10)
 	AND product_brand_name IN ( '湾仔码头', '哈根达斯' )
 	GROUP BY  product_brand_name
 	         ,business_area_name
+	         ,fiscal_month
+), channel_t AS
+(
+	SELECT  product_brand_name
+	       ,case when level_1 = 'EC+HEMA' then '电商' when level_1='Retail' then '零售' else level_1 end as business_area_name
+	       ,fiscal_month
+	       ,nvl(SUM(cases),0) AS sellin_case
+	FROM vw_sales_overview_channel_anal_monthly_flat_qbi -- 渠道分析
+	WHERE date_range_type = 'MTD'
+	AND fiscal_year = 2025
+	AND fiscal_month IN (6, 10)
+	AND product_brand_name IN ( '湾仔码头', '哈根达斯' )
+    and level_1 in ('EC+HEMA', '餐饮', 'BD', '未匹配', 'Retail')
+	GROUP BY  product_brand_name
+	         ,case when level_1 = 'EC+HEMA' then '电商' when level_1='Retail' then '零售' else level_1 end
 	         ,fiscal_month
 )
 SELECT  t1.fiscal_month
@@ -32,8 +47,13 @@ SELECT  t1.fiscal_month
        ,t1.business_area_name
        ,t1.sellin_case
        ,t2.sellin_case AS sellin_case2
-       ,t1.sellin_case - t2.sellin_case as diff
-FROM coverpage AS t1
-LEFT JOIN all AS t2
+       ,t3.sellin_case as sellin_case3
+       ,t1.sellin_case - t2.sellin_case as diff12
+       ,t1.sellin_case - t3.sellin_case as diff13
+FROM OverviewKPI AS t1
+LEFT JOIN all_t AS t2
 ON t1.product_brand_name = t2.product_brand_name AND t1.business_area_name = t2.business_area_name AND t1.fiscal_month = t2.fiscal_month
+left join channel_t as t3
+ON t1.product_brand_name = t3.product_brand_name AND t1.business_area_name = t3.business_area_name AND t1.fiscal_month = t3.fiscal_month
 ORDER BY t1.fiscal_month, t1.product_brand_name, t1.business_area_name
+
--- 01-SalesOverview/SIT/04-DB-未匹配核对.sql
+++ 01-SalesOverview/SIT/04-DB-未匹配核对.sql
@@ -1,6 +1,22 @@
+
+-- overview kpi
 select date_range_type, fiscal_month,business_area_name, sum(cases)
 from vw_sellin_coverpage_monthly_flat_qbi
 where fiscal_year = 2025 and business_area_name in ('BD', '未匹配')
 and fiscal_month <= 10 and date_range_type = 'YTD'
 group by date_range_type, fiscal_month,business_area_name
 order by fiscal_month
+
+
+
+-- 渠道分析
+select product_brand_name, date_range_type, fiscal_month,level_0, level_1, sum(cases) 
+from vw_sales_overview_channel_anal_monthly_flat_qbi
+where fiscal_year = 2025 and product_brand_name in ('哈根达斯', '湾仔码头') and date_range_type = 'YTD'
+and  level_1 in ('BD', '未匹配') and fiscal_month = 10
+group by date_range_type, fiscal_month,level_0, level_1, product_brand_name
+order by product_brand_name, fiscal_month
+
+
+
+-- 进销存总览
