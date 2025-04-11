63f248229ee85edb10809087c9701d2c4449d01d
yuyang <aptx4869yuyang2017@gmail.com>
2025-04-11 15:23:50 +0800

    feat: 销售总览 overview 增加 doh 相关逻辑

--- 01-SalesOverview/Datasource/01-Overview-v2.sql
+++ 01-SalesOverview/Datasource/01-Overview-v2.sql
@@ -1,26 +1,33 @@
-SELECT  t1.date_range_type                                                          AS xtd_type
+SELECT  t1.date_range_type                                                                                  AS union_type
        ,t1.fiscal_year
        ,t1.fiscal_month
-       ,t1.business_area_name
+       ,t1.level_0
+       ,t1.level_1
+       ,t1.level_2
+       ,t1.data_type
        ,t1.product_brand_name
        ,t1.cases
-       ,t1.gsv
+       ,t1.gsv                                                                                              AS gsv_comp
        ,t1.case_ly
-       ,t1.gsv_ly
-       ,t1.le_case
-       ,t1.le_gsv * 1e3                                                             AS le_gsv
-       ,t1.sp_case
-       ,t1.sp_gsv * 1e3                                                             AS sp_gsv
-       ,t1.st_case
-       ,t1.st_gsv * 1e3                                                             AS st_gsv
-       ,t1.fiscal_quarter
+       ,t1.gsv_ly                                                                                           AS gsv_comp_ly
+       ,CASE WHEN level_1 IN ('NKA','RKA','DT','Retail') THEN stock_case_latest
+             WHEN level_1 IN ('餐饮') THEN stock_gsv_incl_promotion_latest END                                AS stock_doh
+       ,CASE WHEN level_1 IN ('NKA','RKA','DT','Retail') THEN sellout_case_latest_nm/days_of_next_fiscal_month
+             WHEN level_1 IN ('餐饮') THEN sellout_gsv_incl_promotion_latest_nm/days_of_next_fiscal_month END AS sellout_doh
+       ,t1.le_case                                                                                          AS le_case_org
+       ,t1.le_gsv * 1e3                                                                                     AS le_gsv_org
+       ,t1.st_case                                                                                          AS st_case_org
+       ,t1.st_gsv * 1e3                                                                                     AS st_gsv_org
+       ,t1.case_total
+       ,t1.gsv_total                                                                                        AS gsv_comp_total
        ,t1.fiscal_yp
        ,t1.fiscal_year_month
        ,t1.fiscal_month_conse
        ,t1.fiscal_quarter_conse
        ,t1.time_pasting
-       ,cast(to_date(substring(created_dt,1,10),'yyyy-mm-dd') AS date)              AS created_dt
-       ,cast(date_add(to_date(substring(created_dt,1,10),'yyyy-mm-dd'),-1) AS date) AS create_dt_yesterday
-FROM vw_sellin_coverpage_monthly_flat_qbi t1
+FROM vw_sales_overview_channel_anal_monthly_flat_qbi AS t1
 WHERE t1.fiscal_year >= 2024
 AND t1.product_brand_name IN ('哈根达斯', '湾仔码头')
+AND t1.level_2 NOT IN ('达上')
+AND level_2 <> '公司调整项'
+AND not (level_1 <> '未匹配' AND level_2 = '未匹配')
